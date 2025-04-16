db01b238bacf8bd5fe0c69fe146d68b3b7c7cd59
Rachel Wu <137188298+rach-w@users.noreply.github.com>
2025-04-16 14:42:19 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+  public static void main(String[] args){
+    //create object of implementation class
+    AirlineQueriesImp b = new AirlineQueriesImp(params);
+    String filename = "";
+    p.loadDataset(filename);
+    //do other functions
+  }
+}
