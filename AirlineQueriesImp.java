8390ebb245d07290cbdec825754c141071a76077
Rachel Wu <137188298+rach-w@users.noreply.github.com>
2025-04-16 14:39:47 -0400

    Create AirlineQueriesImp.java

---
+++ AirlineQueriesImp.java
@@ -0,0 +1,7 @@
+public class AirlineQueriesImp implements AirlineDataQueries{
+  public int loadDataset(String filepath) throws IOException{
+    //code from chat here
+  }
+  //other query searchs implementations below
+  
+}
