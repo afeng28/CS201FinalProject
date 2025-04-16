22c60ba67a19cbff29a3813775377dc77b0240e6
Rachel Wu <137188298+rach-w@users.noreply.github.com>
2025-04-16 14:34:46 -0400

    Update and rename Airlines_interface.java to Airlines.java

---
+++ Airlines.java
@@ -0,0 +1,16 @@
+public class Airlines{
+  private String airport_code;
+  private String name;
+  //etc other variables needed
+
+  public Airlines (params){
+    //constructor
+  }
+  public String getCode(){
+    return airport_code;
+  }
+  //etc other getters and setters
+  //toString code
+}
+
+  
