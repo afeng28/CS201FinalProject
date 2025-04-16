2309c15772af927d4f5e0f813ec9d14583ad56f1
Rachel Wu <137188298+rach-w@users.noreply.github.com>
2025-04-16 14:38:00 -0400

    Create AirlineDataQueries.java

---
+++ AirlineDataQueries.java
@@ -0,0 +1,10 @@
+//interface for data queries
+public interface AirlineDataQueries{
+  //load dataset
+  public int loadDataset(String filePath) throws IOException
+  //exact match query
+  
+  //range query
+
+  //calculate mean
+}
