b71e2c45fa240c1055fe20ed1cdc4acbeb3c90ff
afeng28 <angie.feng@duke.edu>
2025-04-20 11:50:42 -0400

    Update Flights.java

--- Flights.java
+++ Flights.java
@@ -1,4 +1,17 @@
 public static class Flights {
   private String airportCode;
   private String carrierName;
+
+  public Flights (String airportCode, String carrierName) {
+    this.airportCode = airportCode;
+    this.carrierName = carrierName;
+  }
+
+  public String getAirportCode() {
+    return airportCode;
+  }
+
+  public String getCarrierName() {
+    return carrierName;
+  }
 }
