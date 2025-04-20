014fc34584ce1f9098635e7d854b5b5d0a06867f
Rachel Wu <137188298+rach-w@users.noreply.github.com>
2025-04-20 10:45:43 -0400

    Update AirlineDataQueries.java

--- AirlineDataQueries.java
+++ AirlineDataQueries.java
@@ -3,8 +3,34 @@ public interface AirlineDataQueries{
   //load dataset
   public int loadDataset(String filePath) throws IOException
   //exact match query
-  
-  //range query
+  /**
+  * Returns all records that exactly match the specified
+  criteria.
+  * @param attribute The attribute/field to query on
+  * @param value The exact value to match
+  * @return A collection of records matching the criteria
+  */
+  List<T> exactMatchQuery(String attribute, Object value);
+  /**
+  * Returns all records where the specified attribute falls
+  within the given range.
+  * @param attribute The attribute/field to query on
+  * @param lowerBound The lower bound of the range (inclusive)
+  * @param upperBound The upper bound of the range (inclusive)
+  * @return A collection of records matching the criteria
+  */
+  List<T> rangeQuery(String attribute, Comparable lowerBound,
+  Comparable upperBound);
+  // Or
+  List<T> rangeQuery(String attribute, Comparable upperBound);
+  /**
+  * Returns the average value of the specified attribute during
+  a given time frame
+  * @param attribute The attribute/field
+  * @param startTime The start time of the period (inclusive)
+  * @param endTime The end time of the period (inclusive)
+  * @return The statistic calculated */
+  List<T> averageQuery(String attribute, timeStamp startTime,
+  timeStamp endTime);
 
-  //calculate mean
 }
